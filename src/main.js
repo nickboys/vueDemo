717d58b34c1967a2e6f0e2d0e60c40f2fde9a5f0
JianHuangsh <JianHuangsh@quarkfinance.com>
2017-12-07 16:21:47 +0800

    vueDemo

---
+++ src/main.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Administrator on 2017/3/7.
+ */
+//main.js这是项目的核心文件。全局的配置都在这个文件里面配置
+
+import App from './App.vue'
+import router from './routes.js'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Vue from 'vue'
+
+//import './assets/styles/base.css'
+//import './assets/sass/reset.scss'//报错暂时不用sass
+
+Vue.config.debug = true;//开启错误提示
+Vue.use(Element)
+
+new Vue({
+        router,
+        el: '#appIndex',
+        render: h => h(App)
+})
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/routes.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Administrator on 2017/3/7.
+ */
+
+// 引用模板
+import Vue from 'vue';
+import Router from 'vue-router';
+import indexPage from './components/header.vue'
+import homePage from './views/home.vue'
+import aboutPage from './views/about.vue'
+import musicPage from './views/music.vue'
+
+Vue.use(Router)
+
+export default new Router({
+    routes:[
+        {
+            path:'/',
+            component:homePage
+        },
+        {
+            path:'/about',
+            component:aboutPage
+        },
+        {
+            path:'/music',
+            component:musicPage
+        }
+    ]
+})
